49c4eb1f4bacbf2970fd0486c22fa2fa432a3156
Brad Meier <bmeier0689@gmail.com>
2022-05-20 20:46:18 +0900

    Challenge

---
+++ Loop_Challenges.js
@@ -0,0 +1,21 @@
+function printOdds() {
+    for(var i = 0; i<20; i++){
+        if(i % 3 == 0){
+            console.log(i);
+        }
+    }
+}
+
+printOdds()
+
+
+function divideThree() {
+    for(var i = 100; i>0; i--){
+        if(i % 3 == 0){
+            console.log(i);
+        }
+    }
+}
+
+divideThree()
+
