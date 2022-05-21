c78cb4b60ffec1b4765cb932d75026a5b4ea13be
Brad Meier <bmeier0689@gmail.com>
2022-05-21 17:35:00 +0900

    Loop Final

--- Loop_Challenges.js
+++ Loop_Challenges.js
@@ -1,6 +1,7 @@
+// #1
 function printOdds() {
     for(var i = 1; i<20; i++){
-        if(i % 3 == 0){
+        if(i % 2 != 0){
             console.log(i);
         }
     }
@@ -8,7 +9,7 @@ function printOdds() {
 
 printOdds()
 
-
+// #2
 function divideThree() {
     for(var i = 100; i>=0; i--){
         if(i % 3 == 0){
@@ -19,6 +20,7 @@ function divideThree() {
 
 divideThree()
 
+// #3
 function printSequence(arrayInput) {
     for(var i = 0; i<arrayInput.length; i++){
         console.log(arrayInput[i])
@@ -27,22 +29,24 @@ function printSequence(arrayInput) {
 
 printSequence([4,2.5,1,-0.5,-2,-3.5])
 
+// #4
 function sigma() {
     var sum = 0
     for(var i = 1; i<=100; i++){
         sum = sum + i
-        console.log(sum)
     }
+    console.log(sum)
 }
 
 sigma()
 
+// #5
 function factoral() {
     var product = 1
     for(var i = 1; i<=12; i++){
         product = product * i
-        console.log(product)
     }
+    console.log(product)
 }
 
 factoral()
